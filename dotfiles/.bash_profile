916f8f04df41dc89094bf8152b177e0f0bcc3adb
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-07-09 12:07:17 -0700

    Add bashrc

---
+++ dotfiles/.bash_profile
@@ -0,0 +1,3 @@
+export PS1="\[\e[1;32m\][\u@\h:\w]\$\[\e[0m\] "
+export LS_COLORS="di=1;34:ln=1;36:so=1;35:pi=33:ex=1;32:bd=1;33:cd=1;33:su=37;41:sg=30;43:tw=30;42:ow=34;42"
+alias ls="ls --color=always"
--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+shopt -s dotglob
+cp dotfiles/* ~
+
+ID_ED25519_PUB="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL68+PiHa4+qSZNaj3MtqX7ZA3wL9M/2R/lVT2nqo9KS yiyu@galago"
 mkdir -p ~/.ssh
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL68+PiHa4+qSZNaj3MtqX7ZA3wL9M/2R/lVT2nqo9KS yiyu@galago' >>~/.ssh/authorized_keys
+echo "$ID_ED25519_PUB" >>~/.ssh/authorized_keys
 sudo mkdir -p /var/empty
 sudo "$(command -v ssh-keygen)" -A
 sudo "$(command -v sshd)"
