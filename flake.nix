ce29308eade660de9750a4e56a78ea85e9998919
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-07-24 13:57:51 -0700

    Fix nix-index path

--- flake.nix
+++ flake.nix
@@ -4,18 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=master";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      nix-index-database,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        nix-index-db = nix-index-database.packages.${system}.nix-index-with-db;
       in
       {
         packages.default = pkgs.cloudflared;
@@ -23,7 +29,11 @@
           packages = with pkgs; [
             cloudflared
             openssh
+            nix-index-db
           ];
+          shellHook = ''
+            source ${nix-index-db}/etc/profile.d/command-not-found.sh
+          '';
         };
       }
     );
