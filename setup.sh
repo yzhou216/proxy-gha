a5df9d8403beb8883022a87ad582e7102c30243d
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-08-07 02:35:22 -0700

    fixup: remove ssh requirements

--- setup.sh
+++ setup.sh
@@ -4,11 +4,4 @@ set -euo pipefail
 shopt -s dotglob
 cp dotfiles/* ~
 
-ID_ED25519_PUB="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL68+PiHa4+qSZNaj3MtqX7ZA3wL9M/2R/lVT2nqo9KS yiyu@galago"
-mkdir -p ~/.ssh
-echo "$ID_ED25519_PUB" >>~/.ssh/authorized_keys
-sudo mkdir -p /var/empty
-sudo "$(command -v ssh-keygen)" -A
-sudo "$(command -v sshd)"
-
 sleep 5h 58m
